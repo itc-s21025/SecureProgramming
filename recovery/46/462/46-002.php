290f25dcd407f71608a90d4e35bf50948d22f8c5
itc-s21025 <s21025@std.it-college.ac.jp>
2023-09-21 11:03:53 +0000

    46完了

---
+++ recovery/46/462/46-002.php
@@ -0,0 +1,11 @@
+<?php
+  session_start();
+  $session_id = session_id();
+?>
+
+<head>
+<meta name="referrer" content="unsafe-url">
+</head>
+<body>
+  <a href="http://trap.example.com/462/46-900.cgi">外部サイトへのリンク</a><br>
+</body>
