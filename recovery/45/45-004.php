459b9d7e47b70ed4ac2043ae1f2f0d68c62f8037
itc-s21025 <s21025@std.it-college.ac.jp>
2023-09-21 06:06:41 +0000

    45完了

---
+++ recovery/45/45-004.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  $id = filter_input(INPUT_GET, 'id');
+  if (empty($id)) $id = 'yamamoto';
+  $_SESSION['id'] = $id;
+?>
+<body>
+<body>
+<form action="45-005.php" method="post" enctype="multipart/form-data">
+ファイル：<input type="file" name="imgfile" size="20"><br>
+<input type="submit" value="アップロード">
+</form>
+</body>
